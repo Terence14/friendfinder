f99fb049c62f9fe7d319cdf4ed0c9fc9f9bff489
Terence14 <49220084+Terence14@users.noreply.github.com>
2019-07-11 22:24:52 -0700

    needs node modules

---
+++ data/Friends.js
@@ -0,0 +1,45 @@
+var friends = [
+     {
+       
+       name:"Snoop Dogg",
+       photo:"image.png",
+       scores:[]
+
+      },
+      {
+
+        name:"Barack Obama",
+        photo:"image.png",
+        scores:[]
+       }
+       {
+          name:"Michael Jackson",
+          photo:"image.png",
+          scores:[]
+       }
+       {
+         name:"Allen Iverson",
+         photo:"image.png",
+         scores:[]
+
+
+
+       }
+       {
+         name:"Kobe Bryant",
+         photo:"image.png",
+         scores:[]
+       }
+       {
+         name:""
+
+
+
+       }
+
+
+
+
+]
+
+module.exports = friends;
